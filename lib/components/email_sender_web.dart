770877c7c31572f7e24e97361c64027c2e56a0cf
andres <andresangulo90@gmail.com>
2025-01-27 11:23:41 +0100

    debug

--- lib/components/email_sender_web.dart
+++ lib/components/email_sender_web.dart
@@ -3,7 +3,6 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
-
 Future<void> sendEmail(
   { required GlobalKey<FormState> formKey,
     required BuildContext context,
@@ -15,12 +14,23 @@ Future<void> sendEmail(
     required TextEditingController messageController,
     required Function showSuccessDialog,
     required Function setIsSending }) async {
+
   setIsSending(true);
 
   if (formKey.currentState!.validate()) {
+    final backendUrl = dotenv.env['BACKEND_URL'];
+
+    if (backendUrl == null) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Backend URL not set')),
+      );
+      setIsSending(false);
+      return;
+    }
+
     try {
       final response = await http.post(
-        Uri.parse(dotenv.env['BACKEND_URL']!),
+        Uri.parse(backendUrl),
         headers: {
           'Content-Type': 'application/json',
         },
